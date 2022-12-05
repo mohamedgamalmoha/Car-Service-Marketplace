7699071a0767a30b5f73b4022d38743af9a81381
= <=>
2022-12-05 02:18:20 +0200

    Finish info app

--- info/admin.py
+++ info/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MainInfo, Works, AboutUs, TermsOfService, CookiePolicy, PrivacyPolicy, FAQs
+
+
+class ModelAdmin(admin.ModelAdmin):
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        if self.model.objects.count() >= 1:
+            return False
+        return super().has_add_permission(request)
+
+
+admin.site.register(FAQs)
+admin.site.register(Works)
+admin.site.register(AboutUs)
+admin.site.register(CookiePolicy)
+admin.site.register(PrivacyPolicy)
+admin.site.register(TermsOfService)
+admin.site.register(MainInfo, ModelAdmin)
--- info/models.py
+++ info/models.py
@@ -1,3 +1,91 @@
 from django.db import models
+from accounts.models import PhoneNumberValidator
 
-# Create your models here.
+
+class MainInfo(models.Model):
+    facebook = models.URLField('Facebook link')
+    instagram = models.URLField('Instagram link')
+    twitter = models.URLField('Twitter link')
+    telegram = models.URLField('Telegram link', blank=True, null=True)
+
+    mail = models.EmailField('Web email')
+    whatsapp = models.CharField("Whatsapp number", max_length=11, validators=[PhoneNumberValidator, ])
+
+    class Meta:
+        verbose_name = 'Website Main Info'
+        verbose_name_plural = 'Website Main Info'
+
+    def __str__(self):
+        return self.mail
+
+
+class FAQs(models.Model):
+    quote = models.CharField(max_length=1000)
+    answer = models.TextField()
+
+    class Meta:
+        verbose_name = 'FAQ'
+        verbose_name_plural = 'FAQs'
+
+    def __str__(self):
+        return self.quote
+
+
+class AboutUs(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'About us'
+        verbose_name_plural = 'About us'
+
+    def __str__(self):
+        return self.title
+
+
+class TermsOfService(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'Terms Of Service'
+        verbose_name_plural = 'Terms Of Service'
+
+    def __str__(self):
+        return self.title
+
+
+class Works(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'How it works'
+        verbose_name_plural = 'How it works'
+
+    def __str__(self):
+        return self.title
+
+
+class CookiePolicy(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'Cookie Policy'
+        verbose_name_plural = 'Cookie Policy'
+
+    def __str__(self):
+        return self.title
+
+
+class PrivacyPolicy(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'Privacy Policy'
+        verbose_name_plural = 'Privacy Policy'
+
+    def __str__(self):
+        return self.title
