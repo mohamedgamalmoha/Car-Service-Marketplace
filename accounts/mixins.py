ebc844f87818bfddfb6eace79b36d20f3eae5877
= <=>
2023-01-12 19:12:51 +0200

    Update access mixins

--- accounts/mixins.py
+++ accounts/mixins.py
@@ -4,20 +4,25 @@ from .models import UserRole
 
 
 class BaseAuthMixin(AccessMixin):
+
     def has_permission(self, request) -> bool:
         return True
 
     def dispatch(self, request, *args, **kwargs):
-        if self.has_permission(request):
-            return super().dispatch(request, *args, **kwargs)
-        return self.handle_no_permission()
+        if not self.has_permission(request):
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
 
 
-class StaffAuthMixIn(AccessMixin):
+class StaffAuthMixIn(BaseAuthMixin):
+    """Allow admin user only"""
+
     def has_permission(self, request) -> bool:
         return request.user.is_authenticated and request.user.is_superuser
 
 
-class CustomerAuthMixIn(AccessMixin):
+class CustomerAuthMixIn(BaseAuthMixin):
+    """Allow customer user only"""
+
     def has_permission(self, request) -> bool:
         return request.user.is_authenticated and request.user.role == UserRole.CUSTOMER
