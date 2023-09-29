3629e1590b9a3ca36078b5017e51d04acb587f35
= <=>
2023-09-29 02:13:28 +0200

    Fixing error of no such table: info_maininfo

--- core/views.py
+++ core/views.py
@@ -6,18 +6,22 @@ from django.views.generic import TemplateView
 from car.models import Brand
 from booking.models import Discount
 from workshop.models import WorkShop
-from info.models import MainInfo, HeaderImage
 
 
 class HomePage(TemplateView):
     template_name = 'home.html'
-    extra_context = {
-        'brands': Brand.objects.all(),
-        'workshops': WorkShop.objects.all(),
-        'discounts': Discount.objects.all(),
-        'main_info': MainInfo.objects.first(),
-        'header_images': HeaderImage.objects.active(),
-    }
+
+    def get_context_data(self, *args, **kwargs):
+        from info.models import MainInfo, HeaderImage
+        context = super().get_context_data(*args, **kwargs)
+        context.update({
+            'brands': Brand.objects.all(),
+            'workshops': WorkShop.objects.all(),
+            'discounts': Discount.objects.all(),
+            'main_info': MainInfo.objects.first(),
+            'header_images': HeaderImage.objects.active(),
+        })
+        return context
 
 
 def page_not_found_view(request, exception=None):
